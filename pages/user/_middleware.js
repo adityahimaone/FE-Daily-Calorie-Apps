a27da1f7396b0c3f1c9055921aa988d617fd506a
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-03-14 14:06:35 +0700

    Add middleware User

--- pages/user/_middleware.js
+++ pages/user/_middleware.js
@@ -1,12 +1,42 @@
+import { useEffect } from "react";
 import { NextResponse } from "next/server";
+import jwtDecode from "jwt-decode";
+import { clearUser } from "store/userSlice";
+// import { useDispatch } from "react-redux";
+import Cookies from "universal-cookie";
 
 export default function middleware(req) {
   const nextRes = NextResponse;
+  const cookies = new Cookies();
+  // const dispatch = useDispatch();
   const { token } = req.cookies;
 
-  if (!token) {
-    return nextRes.redirect("/login");
-  }
+  let decoded = jwtDecode(token);
 
-  return null;
+  if (!token) {
+    return nextRes.redirect("/");
+  }
+  if (token) {
+    if (!decoded.role !== "user" && decoded.exp * 1000 < Date.now()) {
+      // dispatch(clearUser());
+      cookies.remove("token", {
+        path: "/",
+        domain: window.location.hostname,
+      });
+      nextRes.redirect("/");
+    }
+  }
+  // useEffect(() => {}, [token]);
+
+  console.log(token, decoded);
+
+  // useEffect(() => {
+  //   let decoded = jwtDecode(token);
+  //   console.log(decoded);
+  //   if (!decoded.role !== "user" && decoded.exp * 1000 < Date.now()) {
+  //     // dispatch(clearUser());
+  //     cookies.remove("token", { path: "/", domain: window.location.hostname });
+  //     nextRes.redirect("/");
+  //   }
+  // }, [token]);
 }
