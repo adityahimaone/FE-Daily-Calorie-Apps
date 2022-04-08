672dedb4caceb6ad94ac1651e11c58d2499201cb
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-04-08 22:17:55 +0700

    firebase without service workes

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,11 @@ import AppProvider from "@/context/AppProvider";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "../store/store";
-import * as serviceWorker from "../firebase/serviceWorker";
+// import * as serviceWorker from "../firebase/serviceWorker.js.back";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 const drawerWidth = 240;
-import Router from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      analytics();
-    }
-  }, []);
-
   const theme = createTheme({
     palette: {
       primary: {
@@ -39,18 +32,6 @@ function MyApp({ Component, pageProps }) {
     },
   });
 
-  if (process.env.NODE_ENV !== "production") {
-    Router.events.on("routeChangeError", (err, url) => {
-      if (
-        err.stack.startsWith(
-          "TypeError: Cannot read property 'call' of undefined\n    at __webpack_require__"
-        )
-      ) {
-        location.href = url;
-      }
-    });
-  }
-
   return (
     <>
       <Provider store={store}>
@@ -67,5 +48,5 @@ function MyApp({ Component, pageProps }) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
 export default MyApp;
